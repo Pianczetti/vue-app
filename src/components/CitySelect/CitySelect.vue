7d5726c2187b82c6bdf12529f3f4700a951ff177
Pianczetti <piankapf95@gmail.com>
2023-05-25 11:57:29 +0200

    Przekazywanie id miasta do AddPost

--- src/components/CitySelect/CitySelect.vue
+++ src/components/CitySelect/CitySelect.vue
@@ -1,35 +1,37 @@
 <template>
-    <a-typography-title :level="2" :style="{marginTop: '10px'}">Wybierz grupę</a-typography-title>
-    <a-space>
-      <a-select
-        ref="select"
-        v-model:value="value1"
-        style="width: 120px"
-        :options="options1"
-      ></a-select>
-    </a-space>
-  </template>
-  <script>
-  import { defineComponent, ref } from 'vue';
-  export default defineComponent({
-    setup() {
-      const options1 = ref([{
-        value: 'warszawa',
-        label: 'Warszawa',
-      }, {
-        value: 'krakow',
-        label: 'Kraków',
-      }, {
-        value: 'bialystok',
-        label: 'Białystok',
-      }, {
-        value: 'gdansk',
-        label: 'Gdańsk',
-      }]);
-      return {
-        value1: ref('bialystok'),
-        options1,
-      };
-    },
-  });
-  </script>
+  <a-typography-title :level="2" style="margin-top: 10px"
+    >Wybierz grupę</a-typography-title
+  >
+  <a-space>
+    <a-select
+      ref="select"
+      v-model:value="city"
+      style="width: 120px"
+      :options="options"
+      @change="handleChange"
+    ></a-select>
+  </a-space>
+</template>
+<script>
+import { defineComponent, ref, watch } from "vue";
+import { groups } from "../../storage/groups";
+export default defineComponent({
+  props: ["modelValue"],
+  setup(props, { emit }) {
+    const options = ref([...groups]);
+    const city = ref(props.modelValue || []);
+    const handleChange = value => {
+      city.value = value;
+      emit("update:modelValue", city.value);
+    };
+    watch(city, () => {
+      console.log("value", city.value);
+    });
+    return {
+      options,
+      handleChange,
+      city: ref(3),
+    };
+  },
+});
+</script>
