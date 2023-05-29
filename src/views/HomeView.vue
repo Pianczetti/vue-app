641187f7ea109044103a31d76d77baa4d53bba58
Pianczetti <piankapf95@gmail.com>
2023-05-29 21:20:32 +0200

    Zmiana na composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,80 +1,62 @@
 <template>
   <div class="wrapper">
     <div class="head">
-      <!-- <a-button type="primary" @click="showModal">
+      <button class="btn w-100" @click="showModal">
         <Person />
-      </a-button> -->
-      <!-- <Modal /> -->
-      <button class="btn dp-flex w-100" @click="showModal">
-        <Person :person="activePerson" />
       </button>
-      <AddPost
-        :activePerson="activePerson"
-        :visible="isModalVisible"
-        @close="hideModal"
-        @new-post="addPost"
-      />
+      <a-modal v-model:visible="visible" title="Dodaj Post" @ok="hideModal">
+        <AddPost :visible="visible" @new-post="addPost" @close="hideModal" />
+      </a-modal>
     </div>
-    <PostList :posts="[...posty]" :person="activePerson" />
+    <PostList :posts="posts" :person="activePerson" />
   </div>
 </template>
 
 <script>
-// vendor
-import { ref } from "vue";
-import { Mentionable } from "vue-mention";
-import { kudoses } from "../storage/kudoses.ts";
-import { posts } from "../storage/posts.ts";
+import { ref, onMounted } from "vue";
+import { posts as importedPosts } from "../storage/posts.ts";
 import { people } from "../storage/people";
-//
 import Person from "../components/Person/Person.vue";
-import AddPost from "./AddPost.vue";
-import PostComponent from "../components/Post/Post.vue";
 import PostList from "../components/PostsList/PostList.vue";
 import Agreement from "../components/Icon/Agreement.vue";
-
-const posty = [...posts];
+import AddPost from "./AddPost.vue";
+import { useModal } from "../composables/useModal";
 
 export default {
   components: {
-    PostComponent,
-    AddPost,
     Agreement,
     Person,
     PostList,
+    AddPost,
   },
-  data() {
-    return {
-      isModalVisible: false,
-      posty: posty,
-      kudoses: kudoses,
-      people: people,
-      activePerson: null,
+  setup() {
+    const posts = ref([...importedPosts]);
+    const peopleList = ref(people);
+    const activePerson = ref({});
+    const { visible, showModal, hideModal } = useModal();
+
+    const addPost = dataObject => {
+      posts.value.unshift(dataObject);
     };
-  },
-  methods: {
-    showModal() {
-      this.isModalVisible = true;
-    },
-    hideModal() {
-      this.isModalVisible = false;
-    },
-    addPost(dataObject) {
-      posty.unshift(dataObject);
-      console.log(posty);
-    },
-  },
-  created() {
-    this.activePerson = people.find(person => person.isActive);
+
+    onMounted(() => {
+      activePerson.value = peopleList.value.find(person => person.isActive);
+    });
+
+    return {
+      visible,
+      posts,
+      peopleList,
+      activePerson,
+      showModal,
+      hideModal,
+      addPost,
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-body {
-  max-width: 650px;
-}
-
 ul {
   margin: 0;
   padding: 0;
@@ -84,20 +66,6 @@ li {
   list-style-type: none;
   margin: 0;
   padding: 0;
-}
-
-.mentionable {
-  width: 100%;
-  padding: 10px;
-}
-
-.mention-item {
-  padding: 4px 10px;
-  border-radius: 4px;
-}
-
-.mention-selected {
-  background: rgb(192, 250, 153);
 }
 
 .w-100 {
